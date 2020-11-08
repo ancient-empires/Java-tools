0920753810c148c1beaa58c5df3b6b27f1446e33
ytx21cn <5900105+ytx21cn@users.noreply.github.com>
2020-11-08 14:32:30 -0800

    moved some literals to utils.h

--- Java/AE2lang/AE2lang.c
+++ Java/AE2lang/AE2lang.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-#include <limits.h>
 
 #include "../utils/utils.h"
 
--- Java/utils/utils.h
+++ Java/utils/utils.h
@@ -1,15 +1,21 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <limits.h>
+#include <math.h>
+
 #define BYTE_CAP (size_t)pow(2, CHAR_BIT)
 
 #define ERROR_RW -1
 #define ERROR_ARGS 1
 
+// special characters
 #define VERT '|' // 0x7C
 #define CR '\r' // 0x0D
 #define LF '\n' // 0x0A
 #define CARET '^' // 0x5E
+#define DBQUOTE '"' // 0x22
+#define BACKSLASH '\\' // 0x5C
 
 // Represent an integer with four bytes
 // Modify the four bytes in-place
