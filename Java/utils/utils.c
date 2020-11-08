6c5bc0ba6e26a1e5616ace85873fe3dc97cb9557
ytx21cn <5900105+ytx21cn@users.noreply.github.com>
2020-11-08 15:18:04 -0800

    revised system header inclusion

--- Java/utils/utils.c
+++ Java/utils/utils.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <limits.h>
 
 #include "utils.h"
--- Java/utils/utils.h
+++ Java/utils/utils.h
@@ -1,9 +1,6 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <limits.h>
 #include <math.h>
 
