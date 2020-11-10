8050f441096e0eeb5786c6053b23cae7c816cd05
ytx21cn <5900105+ytx21cn@users.noreply.github.com>
2020-11-10 14:12:42 -0800

    test path

--- Java/AE2pak/test.c
+++ Java/AE2pak/test.c
@@ -3,7 +3,7 @@
 #include "path_processing.h"
 
 int main(void) {
-	char test[] = "///usr//\\\\///bin///sh";
+	char test[] = "///usr/////bin///\\/\\///\\/sh";
 	filepath_t t = splitPath(test);
 	printf("%s %s\n", t.dir, t.filename);
 	freeFilepathStruct(&t);
