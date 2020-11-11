b2ee40458ed761c10572841134095e43c6bf4b7a
ytx21cn <5900105+ytx21cn@users.noreply.github.com>
2020-11-10 19:27:03 -0800

    put aside path processing files

--- Java/AE2pak/path_processing.c
+++ Java/AE2pak/path_processing.c
@@ -1,114 +1,40 @@
-#define _GNU_SOURCE
 #include <string.h>
 #include <stdlib.h>
-#include <limits.h>
 
 #include "../utils/utils.h"
 #include "path_processing.h"
 
-// Normalize path (path to normalize must exist).
-// Return pointer to the normalized path.
-char* normalizePath(char* path) {
-	char* normalized = canonicalize_file_name(path);
-	if (normalized) {
-		free(path);
-		path = normalized;
-	}
-	return path;
-}
-
-// Convert Windows to Unix path, and return pointer to the converted path.
+// Convert Windows to Unix path (modify in-place), and return pointer to the converted path.
 char* Windows2UnixPath(char* path) {
 	strrep(path, BACKSLASH, SLASH);
-	path = normalizePath(path);
 	return path;
 }
 
-// Convert Unix to Windows path, and return pointer to the converted path.
+// Convert Unix to Windows path (modify in-place), and return pointer to the converted path.
 char* Unix2WindowsPath(char* path) {
-	path = normalizePath(path);
 	strrep(path, SLASH, BACKSLASH);
 	return path;
 }
 
-// Split path into directory and filename.
-// Automaically convert to Unix path (separator is '/') in the struct, but not in the original path.
-// After conversion, the directory will not contain the trailing "/" character.
-filepath_t splitPath(char* path) {
+// Get filename from path, and modify path in-place.
+char* getFilename(char** pPath) {
 
-	filepath_t filepath;
+	Windows2UnixPath(*pPath);
 
-	// copy path string, and convert to Unix path
-	size_t pathLen = strlen(path);
-	char* buffer = calloc(pathLen + 1, sizeof(char));
-	strcpy(buffer, path);
-	buffer = Windows2UnixPath(buffer);
-
-	// Find last occurence of '/'.
-	// This is the separation of directory and filename.
-	char* lastSlash = strrchr(buffer, SLASH);
+	char* lastSlash = strrchr(*pPath, SLASH);
 	if (!lastSlash) {
-		// If '/' does not exist, then directory is '.'
-		// save directory name
-		char dir[] = ".";
-		filepath.dir = calloc(strlen(dir) + 1, sizeof(char));
-		strcpy(filepath.dir, dir);
-
-		// save filename
-		char* filename = buffer;
-		filepath.filename = calloc(pathLen + 1, sizeof(char));
-		strcpy(filepath.filename, filename);
-	}
-	else {
-		// Save directory name before '/'
-		ssize_t dirLen = lastSlash - buffer;
-		if (dirLen < 1) {
-			// Edge case: directory is just "/"
-			// save directory name`
-			char dir[] = "/";
-			filepath.dir = calloc(strlen(dir) + 1, sizeof(char));
-			strcpy(filepath.dir, dir);
-
-			// save filename
-			char* filename = &buffer[1];
-			filepath.filename = calloc(pathLen, sizeof(char));
-			strcpy(filepath.filename, filename);
-		}
-		else {
-			// save directory name
-			*lastSlash = '\0';
-			char* dir = buffer;
-			size_t dirLen = strlen(dir);
-			// trace back to remove redundant slashes
-			ssize_t i = dirLen - 1;
-			for (; i > 0; --i) {
-				if (dir[i] == SLASH) {
-					dir[i] = '\0';
-				}
-				else {
-					break;
-				}
-			}
-			dirLen = i + 1;
-			filepath.dir = calloc(dirLen + 1, sizeof(char));
-			strcpy(filepath.dir, dir);
-
-			// save filename
-			char* filename = &lastSlash[1];
-			size_t filenameLen = strlen(filename);
-			filepath.filename = calloc(filenameLen + 1, sizeof(char));
-			strcpy(filepath.filename, filename);
-		}
+		// if slash is not found, then just use the whole path as filename
+		return *pPath;
 	}
 
-	free(buffer);
-	return filepath;
-}
+	// find start of filename (after the last '/')
+	char* filenameStart = lastSlash + 1;
 
-// Free the filepath struct created by splitPath.
-void freeFilepathStruct(filepath_t* pFilepath) {
-	free(pFilepath->dir);
-	pFilepath->dir = NULL;
-	free(pFilepath->filename);
-	pFilepath->filename = NULL;
+	// overwrite original path
+	size_t filenameLen = strlen(filenameStart);
+	memmove(*pPath, filenameStart, filenameLen);
+
+	// reallocate path
+	*pPath = realloc(*pPath, filenameLen * sizeof(char));
+	return *pPath;
 }
--- Java/AE2pak/path_processing.h
+++ Java/AE2pak/path_processing.h
@@ -3,27 +3,13 @@
 
 #include <string.h>
 
-typedef struct {
-	char* dir;
-	char* filename;
-} filepath_t;
-
-// Normalize path (path to normalize must exist).
-// Return pointer to the normalized path.
-char* normalizePath(char* path);
-
-// Convert Windows to Unix path, and return pointer to the converted path.
+// Convert Windows to Unix path (modify in-place), and return pointer to the converted path.
 char* Windows2UnixPath(char* path);
 
-// Convert Unix to Windows path, and return pointer to the converted path.
+// Convert Unix to Windows path (modify in-place), and return pointer to the converted path.
 char* Unix2WindowsPath(char* path);
 
-// Split path into directory and filename.
-// Automaically convert to Unix path (separator is '/') in the struct, but not in the original path.
-// After conversion, the directory will not contain the trailing "/" character.
-filepath_t splitPath(char* path);
-
-// Free the filepath struct created by splitPath.
-void freeFilepathStruct(filepath_t* filepath);
+// Get filename from path, and modify path in-place.
+char* getFilename(char** pPath);
 
 #endif
