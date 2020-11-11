2ab24d37e979e82f530a9874ab5c3901589ff232
ytx21cn <5900105+ytx21cn@users.noreply.github.com>
2020-11-10 19:50:48 -0800

    separated extract.c extract.h

---
+++ Java/AE2pak/extract.c
@@ -0,0 +1,162 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "../utils/utils.h"
+#include "system.h"
+#include "extract.h"
+
+#define LARGE_SPACE_SIZE 2048
+
+// Extract single file (internal use only)
+static bool extract_file(const char* fo2s, char* fn2s, unsigned int fileDataPos, unsigned int fileSize) {
+	FILE *fo2, *fn2;
+	unsigned int i;
+	unsigned char c1;
+
+	fo2 = fopen(fo2s, "rb");
+	if (!fo2) {
+		fprintf(stderr, "ERROR: Could not open \"%s\" for extraction!\n", fo2s);
+		return false;
+	}
+	rewind(fo2);
+	fseek(fo2, fileDataPos, 0);
+
+	fn2 = fopen(fn2s, "wb");
+	if (!fn2) {
+		fprintf(stderr, "ERROR: Could not open \"%s\" for writing !\n", fn2s);
+		fclose(fo2);
+		return false;
+	}
+
+	for (i = 0; i < fileSize; ++i) {
+		c1 = getc(fo2);
+		if (feof(fo2)) {
+			fprintf(stderr, "ERROR: EOF reached when extracting!\n Check your file!");
+			fclose(fo2);
+			fclose(fn2);
+			return false;
+		}
+		putc(c1, fn2);
+	}
+
+	fclose(fo2);
+	fclose(fn2);
+	return true;
+}
+
+// Extract .pak file to extract directory.
+// Write the .log file containing the list of all extracted files in current working directory.
+void extract(const char* pakFile, const char* extractDir) {
+
+	printf("Extracting...\n");
+
+	// open .pak file for reading
+	mkdir(extractDir, MKDIR_DEFAULT_MODE);
+	FILE* pakFileDesc = fopen(pakFile, "rb");
+	if (!pakFileDesc) {
+		fprintf(stderr, "ERROR: PAK file \"%s\" not found.\n", pakFile);
+		fclose(pakFileDesc);
+		exit(ERROR_RW);
+	}
+
+	// get .log file in which to write extracted file list
+	// place it in current working directory
+	char fileListLOG[LARGE_SPACE_SIZE];
+	strcpy(fileListLOG, "_filelist.log");
+	printf("\nStoring file list in log file: \"%s\"\n\n", fileListLOG);
+
+	// open the file list .log file for writing
+	FILE* fileListDesc = fopen(fileListLOG, "wb");
+	if (!fileListDesc) {
+		fprintf(stderr, "ERROR: \"%s\" cannot be created for writing.\n", fileListLOG);
+		fclose(pakFileDesc);
+		fclose(fileListDesc);
+		exit(ERROR_RW);
+	}
+
+	unsigned char c1, c2, c3, c4;
+
+	// get starting position of file data (first 2 bytes)
+	rewind(pakFileDesc);
+	c1 = getc(pakFileDesc);
+	c2 = getc(pakFileDesc);
+	unsigned int fileDataStartPos = fourBytesToUnsignedInt(0, 0, c1, c2);
+	printf("File data starts at byte: %d\n", fileDataStartPos);
+
+	// get number of total files (next 2 bytes)
+	c1 = getc(pakFileDesc);
+	c2 = getc(pakFileDesc);
+	unsigned int totalFiles = fourBytesToUnsignedInt(0, 0, c1, c2);
+	printf("Total Files announced: %d\n", totalFiles);
+
+	// check unexpected ending of .pak file
+	if (feof(pakFileDesc)) {
+		fclose(pakFileDesc);
+		fprintf(stderr, "ERROR: Unexpected end of .pak file:\n\"%s\"\n", pakFile);
+		fclose(pakFileDesc);
+		fclose(fileListDesc);
+		exit(ERROR_RW);
+	}
+
+	// process all files
+
+	unsigned int totalExtracted = 0, totalErrors = 0;
+
+	for (unsigned int i = 0; i < totalFiles; ++i) {
+		// get filename length (2 bytes)
+		c1 = getc(pakFileDesc);
+		c2 = getc(pakFileDesc);
+		unsigned int filenameLen = fourBytesToUnsignedInt(0, 0, c1, c2);
+
+		// get filename
+		unsigned int j = 0;
+		char filename[LARGE_SPACE_SIZE];
+		for (; j < filenameLen; ++j) {
+			filename[j] = getc(pakFileDesc);
+		}
+		filename[j] = 0;
+
+		// get file data position (4 bytes)
+		c1 = getc(pakFileDesc);
+		c2 = getc(pakFileDesc);
+		c3 = getc(pakFileDesc);
+		c4 = getc(pakFileDesc);
+		unsigned int fileDataPos = fourBytesToUnsignedInt(c1, c2, c3, c4) + fileDataStartPos;
+
+		// get file size (2 bytes)
+		c1 = getc(pakFileDesc);
+		c2 = getc(pakFileDesc);
+		unsigned int fileSize = fourBytesToUnsignedInt(0, 0, c1, c2);
+
+		// write to file list
+		char line[LARGE_SPACE_SIZE];
+		size_t extractDirLen = strlen(extractDir);
+		strcpy(line, extractDir);
+		if (line[extractDirLen-1] == DBQUOTE) {
+			line[extractDirLen-1]=0;
+		}
+		else if (line[extractDirLen-1] == BACKSLASH) {
+			line[extractDirLen-1]=0;
+		}
+		strcat(line, "/");
+		strcat(line, filename);
+		fprintf(fileListDesc, "%s\n", line);
+
+		// Extract file
+		if (!extract_file(pakFile, line, fileDataPos, fileSize)) {
+			++totalErrors;
+		}
+		++totalExtracted;
+	}
+
+	// finish
+	fclose(pakFileDesc);
+	fclose(fileListDesc);
+
+	printf("\nUh yeah, it's done!\n");
+	printf("Total files: %d\n", totalFiles);
+	printf("Total extracted: %d\n", totalExtracted);
+	printf("Total errors: %d\n", totalErrors);
+}
---
+++ Java/AE2pak/extract.h
@@ -0,0 +1,8 @@
+#ifndef EXTRACT_H
+#define EXTRACT_H
+
+// Extract .pak file to extract directory.
+// Write the .log file containing the list of all extracted files in current working directory.
+void extract(const char* pakFile, const char* extractDir);
+
+#endif
