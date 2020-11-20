b65a81ad9ded4a88084498204a527cd6728aced9
ytx21cn <5900105+ytx21cn@users.noreply.github.com>
2020-11-20 12:44:16 -0800

    error handling for too large total resource file size

--- Java/AE2pak/pack.c
+++ Java/AE2pak/pack.c
@@ -14,14 +14,15 @@
 // Check all the files that are listed in the file list .log file
 // If failed to open the file list, or if one or more errors encountered when checking resource files, then abort the program with exit code ERROR_RW.
 // Otherwise, return total number of files that are successfully checked.
-static unsigned int checkAllFiles(const char* fileListLOG, unsigned int* pTotalFiles, unsigned int *pTotalErrors, unsigned int *pTotalFileInfoLen) {
+static unsigned int checkAllFiles(const char* fileListLOG, unsigned int* pTotalResourceFiles, unsigned int *pTotalErrors, unsigned int *pTotalFileInfoLen) {
 
 	printf("Checking all files...\n\n");
 
-	*pTotalFiles = 0;
+	*pTotalResourceFiles = 0;
 	*pTotalErrors = 0;
-
 	*pTotalFileInfoLen = FILE_DATA_START_POS_BYTES + TOTAL_NUM_FILES_BYTES;
+
+	unsigned long totalResourceFileSize = 0;
 
 	// open the .log file
 	FILE* fileListDesc = fopen(fileListLOG, "r");
@@ -70,10 +71,13 @@ static unsigned int checkAllFiles(const char* fileListLOG, unsigned int* pTotalF
 		}
 		else {
 			// file is valid, and file size is valid
-			++(*pTotalFiles);
+			++(*pTotalResourceFiles);
+
 			fileinfo_t fileInfo = saveFileInfo(filePath, fileSize);
 			unsigned int fileInfoLen = getFileInfoLen(&fileInfo);
 			*pTotalFileInfoLen += fileInfoLen;
+
+			totalResourceFileSize += fileSize;
 		}
 
 		free(filePath);
@@ -85,40 +89,45 @@ static unsigned int checkAllFiles(const char* fileListLOG, unsigned int* pTotalF
 		exit(ERROR_RW);
 	}
 	else if (*pTotalFileInfoLen >= FILE_DATA_START_POS_MAX) {
-		fprintf(stderr, "ERROR: File info is too long (%u bytes).\n", *pTotalFileInfoLen);
-		fprintf(stderr, "The maximum allowed length of file info is %u bytes.\n", FILE_DATA_START_POS_MAX - 1);
+		fprintf(stderr, "ERROR: File info is too long (%u bytes)\n", *pTotalFileInfoLen);
+		fprintf(stderr, "Maximum allowed length of file info: %u bytes.\n", FILE_DATA_START_POS_MAX - 1);
 		fprintf(stderr, "Please reduce the length of filenames, and then try again.\n\n");
 		exit(ERROR_RW);
 	}
-	else if (*pTotalFiles > TOTAL_NUM_FILES_MAX) {
-		fprintf(stderr, "ERROR: Too many resource files to pack.\n");
-		fprintf(stderr, "You have %u resource files.\n", *pTotalFiles);
-		fprintf(stderr, "The .pak archive can only hold a maximum of %u resource files.\n\n", TOTAL_NUM_FILES_MAX);
+	else if (*pTotalResourceFiles > TOTAL_NUM_FILES_MAX) {
+		fprintf(stderr, "ERROR: Too many resource files to pack (%u files)\n", *pTotalResourceFiles);
+		fprintf(stderr, "Maximum allowed number of resource files: %u.\n\n", TOTAL_NUM_FILES_MAX);
+		exit(ERROR_RW);
+	}
+	else if (totalResourceFileSize > FILE_DATA_START_OFFSET_MAX) {
+		fprintf(stderr, "ERROR: Total size of resource files is too large (%lu bytes)\n", totalResourceFileSize);
+		fprintf(stderr, "Maximum allowed total size of resource files: %u bytes.\n", FILE_DATA_START_OFFSET_MAX);
+		fprintf(stderr, "Please try again with less files to pack.\n\n");
 		exit(ERROR_RW);
 	}
 	else {
-		printf("Successfully checked %u files.\n", *pTotalFiles);
+		printf("Successfully checked %u files.\n", *pTotalResourceFiles);
 		printf("Total errors: %u\n", *pTotalErrors);
 		printf("Total file info length: %u\n\n", *pTotalFileInfoLen);
-		return *pTotalFiles;
+		return *pTotalResourceFiles;
 	}
 }
 
 // Create the .pak archive, using files specified in the file list .log file.
 void pack(const char* pakFile, const char* fileListLOG) {
-	unsigned int totalFiles = 0;
+	unsigned int totalResourceFiles = 0;
 	unsigned int totalErrors = 0;
 	unsigned int totalFileInfoLen = 0;
 
 	printf("Packing...\n\n");
 
 	// check all files present in the file list .log file.
-	checkAllFiles(fileListLOG, &totalFiles, &totalErrors, &totalFileInfoLen);
+	checkAllFiles(fileListLOG, &totalResourceFiles, &totalErrors, &totalFileInfoLen);
 
 	// process all files
-	fileinfo_t* allFilesInfo = calloc(totalFiles, sizeof(fileinfo_t));
+	fileinfo_t* allResourceFilesInfo = calloc(totalResourceFiles, sizeof(fileinfo_t));
 
-	free(allFilesInfo);
+	free(allResourceFilesInfo);
 
-	printf("\nUh yeah, its done! %u errors for %u announced files.\n\n", totalErrors, totalFiles);
+	printf("\nUh yeah, its done! %u errors for %u announced files.\n\n", totalErrors, totalResourceFiles);
 }
