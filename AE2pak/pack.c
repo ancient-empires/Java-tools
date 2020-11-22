c4f12b205244eb3c217e0b027ce6ca01edac0704
ytx21cn <5900105+ytx21cn@users.noreply.github.com>
2020-11-21 18:26:54 -0800

    added comments: CURRENT WORKING DIRECTORY

--- AE2pak/pack.c
+++ AE2pak/pack.c
@@ -10,7 +10,7 @@
 #include "pack.h"
 
 // Get the file paths in the file list .log file.
-// In the file list .log file, each line contains a single path.
+// In the file list .log file, each line contains a single path. Each relative path must be relative to the CURRENT WORKING DIRECTORY.
 // This function allocates a new string to store the file path.
 // The user must call free() to free the memory afterwards.
 static char* getFilePath(FILE* fileListDesc) {
@@ -203,6 +203,7 @@ static void copyData(FILE* pakFileDesc, const fileinfo_t* allResourceFilesInfo, 
 }
 
 // Create the .pak archive, using file paths specified in the file list .log file.
+// The .log file contains the paths to the resource files to be added to the .pak. These paths are relative to the CURRENT WORKING DIRECTORY.
 void pack(const char* pakFile, const char* fileListLOG) {
 	unsigned int totalResourceFiles = 0;
 	unsigned int totalErrors = 0;
--- AE2pak/pack.h
+++ AE2pak/pack.h
@@ -4,6 +4,7 @@
 #include "pak_limits.h"
 
 // Create the .pak archive, using file paths specified in the file list .log file.
+// The .log file contains the paths to the resource files to be added to the .pak. These paths are relative to the CURRENT WORKING DIRECTORY.
 void pack(const char* pakFile, const char* fileListLOG);
 
 #endif
