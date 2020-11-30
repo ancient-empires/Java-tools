30482808b8b637a440ef8caf305197e8af7b0f8a
ytx21cn <5900105+ytx21cn@users.noreply.github.com>
2020-11-29 20:11:17 -0800

    removed unnecessary files

--- AE2pak/pack_backup.h
+++
@@ -1,9 +0,0 @@
-#ifndef PACK_H
-#define PACK_H
-
-#include "pak_limits.h"
-
-// Create the .pak archive, using files specified in the file list.
-void pack(const char* pakFile, const char* fileListLOG);
-
-#endif
