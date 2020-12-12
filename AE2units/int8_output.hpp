1a5ed8e8477c246f3df0b9e007727b265d956d68
ytx21cn <5900105+ytx21cn@users.noreply.github.com>
2020-12-12 13:31:02 -0800

    comments revision

--- AE2units/int8_output.hpp
+++ AE2units/int8_output.hpp
@@ -4,6 +4,9 @@
 #ifndef INT8_OUTPUT_HPP
 #define INT8_OUTPUT_HPP
 
+// This header file overloads the << operator.
+// Hence the int8_t and uint8_t integers are printed as digits.
+
 std::ostream& operator<<(std::ostream& outputStream, const int8_t& num);
 
 std::ostream& operator<<(std::ostream& outputStream, const uint8_t& num);
