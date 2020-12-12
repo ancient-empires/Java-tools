235d3153a1c734379698c6375f53102f8cb482c7
ytx21cn <5900105+ytx21cn@users.noreply.github.com>
2020-12-12 03:06:10 -0800

    use static for overloaded << operators

--- AE2units/UnitProcessor.cpp
+++ AE2units/UnitProcessor.cpp
@@ -12,12 +12,12 @@ extern "C" {
 
 typedef std::pair<int8_t, int8_t> charpos;
 
-std::ostream& operator<<(std::ostream& outputStream, const int8_t& num) {
+static std::ostream& operator<<(std::ostream& outputStream, const int8_t& num) {
 	outputStream << static_cast<int>(num);
 	return outputStream;
 }
 
-std::ostream& operator<<(std::ostream& outputStream, const uint8_t& num) {
+static std::ostream& operator<<(std::ostream& outputStream, const uint8_t& num) {
 	outputStream << static_cast<unsigned int>(num);
 	return outputStream;
 }
